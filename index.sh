05cbf9c09c958c72bf92fe1c4a1759e7e4e82069
Robert Kampas <robert.kampas@interpublic.com>
2026-02-17 11:29:49 +0000

    Added script to automatically generate files index.

---
+++ index.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+{
+    echo "# Index"
+
+    find . -mindepth 1 -not -path './.git*' -not -name 'README.md' -not -name '.DS_Store' | sort | while read -r item; do
+        relative="${item#./}"
+        name=$(basename "$relative")
+        depth=$(echo "$relative" | tr -cd '/' | wc -c)
+        indent=$(printf '%*s' $((depth * 4)) '')
+
+        if [ -d "$item" ]; then
+            title=$(echo "$name" | sed 's/-/ /g; s/\b\(.\)/\u\1/g')
+            echo "${indent}* [${title}](${relative}/)"
+        else
+            echo "${indent}* [${name}](${relative})"
+        fi
+    done
+} > README.md
+
+echo "README.md generated."
