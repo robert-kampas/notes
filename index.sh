21d4afeed1a0c1a5c776d4cb99e0836c9668a09a
Robert Kampas <robert.kampas@interpublic.com>
2026-02-17 11:32:32 +0000

    Improved script to generate files index.

--- index.sh
+++ index.sh
@@ -1,21 +1,20 @@
 #!/bin/bash
-
 {
     echo "# Index"
-
-    find . -mindepth 1 -not -path './.git*' -not -name 'README.md' -not -name 'index.sh' -not -name '.DS_Store' | sort | while read -r item; do
-        relative="${item#./}"
-        name=$(basename "$relative")
-        depth=$(echo "$relative" | tr -cd '/' | wc -c)
-        indent=$(printf '%*s' $((depth * 4)) '')
-
-        if [ -d "$item" ]; then
-            title=$(echo "$name" | sed 's/-/ /g; s/\b\(.\)/\u\1/g')
-            echo "${indent}* [${title}](${relative}/)"
-        else
-            echo "${indent}* [${name}](${relative})"
-        fi
-    done
+    echo ""
+    find . -mindepth 1 -not -path './.git*' -not -name 'README.md' -not -name 'index.sh' -not -name '.DS_Store' -print0 |
+        sort -z |
+        while IFS= read -r -d '' item; do
+            relative="${item#./}"
+            name=$(basename "$relative")
+            depth=$(echo "$relative" | tr -cd '/' | tr -d ' ' | wc -c | tr -d ' ')
+            indent=$(printf '%*s' $((depth * 4)) '')
+            if [ -d "$item" ]; then
+                title=$(echo "$name" | perl -pe 's/-/ /g; s/\b(\w)/\u$1/g')
+                echo "${indent}* [${title}](${relative}/)"
+            else
+                echo "${indent}* [${name}](${relative})"
+            fi
+        done
 } > README.md
-
 echo "README.md generated."
